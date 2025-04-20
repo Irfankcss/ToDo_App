2ce27ee59fb4b68b71d7a668aa68a52b1fc79965
irfankcss <irfankuric@gmail.com>
2025-04-20 19:02:20 +0200

    taskd

--- db/migrate/20250420142914_add_timestamp_to_tasks.rb
+++ db/migrate/20250420142914_add_timestamp_to_tasks.rb
@@ -1,4 +1,4 @@
-class AddTimestampToTasks < ActiveRecord::Migration[8.0]
+class AddTimestampToTasks < ActiveRecord::Migration[7.1]
   def change
     add_column :tasks, :timestamp, :datetime
     change_column_default :tasks, :timestamp, -> { 'CURRENT_TIMESTAMP' }
